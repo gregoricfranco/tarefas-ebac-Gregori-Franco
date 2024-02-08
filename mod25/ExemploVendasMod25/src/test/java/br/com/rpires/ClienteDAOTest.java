e98f463753f59110b8187a0b5bf6fcb407620a15
Gregori Franco <gregorifranco@gmail.com>
2024-02-08 17:14:43 -0300

    atividade projeto 1 parte 3

---
+++ mod25/ExemploVendasMod25/src/test/java/br/com/rpires/ClienteDAOTest.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package br.com.rpires;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.rpires.dao.ClienteDAO;
+import br.com.rpires.dao.IClienteDAO;
+import br.com.rpires.domain.Cliente;
+import br.com.rpires.exceptions.TipoChaveNaoEncontradaException;
+ClienteDAOTest {
+	
+	private IClienteDAO clienteDao;
+
+	private Cliente cliente;
+	
+	public ClienteDAOTest() {
+		clienteDao = new ClienteDAO();
+	}
+	
+	@Before
+	public void init() throws TipoChaveNaoEncontradaException {
+		cliente = new Cliente();
+		cliente.setCpf(12312312312L);
+		cliente.setNome("Gregori");
+		cliente.setCidade("São Jose ");
+		cliente.setEnd("Endereco");
+		cliente.setEstado("sc");
+		cliente.setNumero(20);
+		cliente.setTel(4899999999L);
+		clienteDao.cadastrar(cliente);
+	}
+	
+	@Test
+	public void pesquisarCliente() {
+		Cliente clienteConsultado = clienteDao.consultar(cliente.getCpf());
+		Assert.assertNotNull(clienteConsultado);
+	}
+	
+	@Test
+	public void salvarCliente() throws TipoChaveNaoEncontradaException {
+		cliente.setCpf(56565656565L);
+		Boolean retorno = clienteDao.cadastrar(cliente);
+		Assert.assertTrue(retorno);
+	}
+	
+	
+	@Test
+	public void excluirCliente() {
+		clienteDao.excluir(cliente.getCpf());
+	}
+	
+	@Test
+	public void alterarCliente() throws TipoChaveNaoEncontradaException {
+		cliente.setNome("Rodrigo Pires");
+		clienteDao.alterar(cliente);
+		Assert.assertEquals("Rodrigo Pires", cliente.getNome());
+	}
+	
+	@Test
+	public void buscarTodos() {
+		Collection<Cliente> list = clienteDao.buscarTodos();
+		assertTrue(list != null);
+		assertTrue(list.size() == 2);
+	}
+}
